3ec5ba0f403e552181bf11987163238bca3be712
aczwink <amir130@hotmail.de>
2025-11-01 13:32:10 +0100

    Update main.ts

--- translator/src/main.ts
+++ translator/src/main.ts
@@ -40,7 +40,6 @@ async function CallTranslationService(texts: string[], targetLanguage: TargetTra
     };
 
     const sender = new HTTP.RequestSender;
-    console.log(headers);
     const response = await sender.SendRequest({
         body,
         headers,
